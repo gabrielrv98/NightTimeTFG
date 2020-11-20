9768b8ce34ec0c4a92516109cfe22ec3eeaa2727
minimerlyn <gabrielrv98@gmail.com>
2020-11-20 20:36:05 +0100

    buttons created

--- Start/app/src/main/java/com/esei/grvidal/nighttime/pages/ProfileEditor.kt
+++ Start/app/src/main/java/com/esei/grvidal/nighttime/pages/ProfileEditor.kt
@@ -4,13 +4,24 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.rounded.Cancel
+import androidx.compose.material.icons.sharp.Cancel
+import androidx.compose.material.icons.twotone.Cancel
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.VectorAsset
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavHostController
@@ -22,69 +33,137 @@ import com.esei.grvidal.nighttime.data.ProfileViewModel
 import com.esei.grvidal.nighttime.data.User
 import com.esei.grvidal.nighttime.data.meUser
 import androidx.lifecycle.ViewModel
+import java.util.stream.Stream
 
 @Composable
-fun ProfileEditorPage(navController : NavHostController){
+fun ProfileEditorPage(navController: NavHostController) {
     val goBack = {
         navController.popBackStack(navController.graph.startDestination, false)
         navController.navigate(BottomNavigationScreens.Profile.route)
     }
 
-    var name by remember { mutableStateOf(TextFieldValue(meUser.name))}
-    var status by remember { mutableStateOf(TextFieldValue(meUser.status))}
+    var name by remember { mutableStateOf(TextFieldValue(meUser.name)) }
+    var status by remember { mutableStateOf(TextFieldValue(meUser.status)) }
 
-    Column{
+    Column {
         Header(text = stringResource(R.string.edit_profile))
+        AcceptDeclineButtons(
+            accept = {
+                saveData(user = meUser, name = name, status = status)
+                goBack()
+            },
+            decline = { goBack() }
+        )
 
         TextChanger(
             name = name,
-            set = {text -> name = text},
-            text = stringResource(id = R.string.display_name))
+            set = { text -> name = text },
+            text = stringResource(id = R.string.display_name)
+        )
 
         TextChanger(
             name = status,
-            set = {text -> status = text},
-            text = stringResource(id = R.string.status))
+            set = { text -> status = text },
+            canBeEmpty = true,
+            text = stringResource(id = R.string.status)
+        )
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ){
-            Button(onClick = goBack
-            ) {
-                Text("Cancelar")
+
+    }
+
+}
+
+
+@Composable
+private fun AcceptDeclineButtons(
+    accept: () -> Unit,
+    decline: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButtonEditProfile(
+            Icons.Outlined.Cancel,
+            decline,
+            stringResource(id = R.string.cancelar)
+        )
+
+        IconButtonEditProfile(Icons.Outlined.Check, accept, stringResource(id = R.string.aceptar))
+    }
+}
+
+@Composable
+private fun IconButtonEditProfile(
+    icon: VectorAsset,
+    accept: () -> Unit,
+    text: String = ""
+) {
+    IconButton(
+        modifier = Modifier.padding(horizontal = 12.dp)
+            .padding(bottom = 12.dp),
+        onClick = accept
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(asset = icon)
+            ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                Text(
+                    text,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontSize = 10.sp
+
+                )
             }
 
-            Button(onClick = {
-                saveData(user = meUser, name = name, status = status)
-                goBack()
-            }
-            ) {
-                Text("Aceptar")
-            }
         }
 
     }
 }
 
-fun saveData(user: User, name: TextFieldValue, status : TextFieldValue){
+fun saveData(user: User, name: TextFieldValue, status: TextFieldValue) {
 
-    user.name = name.text
-    user.status = status.text
+    if (name.text.trim() != "")
+        user.name = name.text.trim()
+    user.status = status.text.trim()
 }
 
 @Composable
-fun TextChanger(name: TextFieldValue, set : (TextFieldValue) -> Unit, text : String){
+fun TextChanger(
+    name: TextFieldValue,
+    set: (TextFieldValue) -> Unit,
+    canBeEmpty: Boolean = false,
+    text: String
+) {
 
     Row(
         modifier = Modifier.padding(horizontal = 24.dp)
-            .padding(bottom = 6.dp)
-    ){
-        Column{
-            Text( text = text)
+            .padding(bottom = 6.dp, top = 6.dp)
+    ) {
+        Column {
+            Row{
+                Text(text = text)
+                if (!canBeEmpty){ Advert( name.text, Modifier.align(Alignment.Bottom)) }
+            }
+
             TextField(
-                modifier = Modifier.padding(horizontal = 4.dp),
-                value = name, onValueChange = { set(it)} )
+                modifier = Modifier.padding(horizontal = 6.dp)
+                    .padding(top = 4.dp),
+                value = name, onValueChange = { set(it) })
         }
+    }
+}
+
+@Composable
+fun Advert( text: String, modifier :Modifier ){
+    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+        Text(
+            modifier = modifier.padding(start = 6.dp)
+                ,
+            style = MaterialTheme.typography.subtitle1,
+            fontSize = 12.sp,
+            text = "*" + stringResource(id = R.string.cant_be_empty),
+            color = if (text.isBlank()) MaterialTheme.colors.error else MaterialTheme.colors.onSurface
+        )
     }
 }
