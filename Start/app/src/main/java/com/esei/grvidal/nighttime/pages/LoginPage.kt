b056f07c0e8cdea5b8958fd07a110c6f1bc82987
Gabriel Rguez Vidal <gabrielrv98@gmail.com>
2021-02-19 01:58:15 +0100

    Improves clarity of code

--- Start/app/src/main/java/com/esei/grvidal/nighttime/pages/LoginPage.kt
+++ Start/app/src/main/java/com/esei/grvidal/nighttime/pages/LoginPage.kt
@@ -5,51 +5,84 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-
+import com.esei.grvidal.nighttime.R
 
 @Composable
 fun LoginPage() {
-    val username = remember { mutableStateOf("") }
-    val password = remember { mutableStateOf("") }
+    val (username,setUsername) = remember { mutableStateOf("") }
+    val (password,setPassword) = remember { mutableStateOf("") }
 
-    LoginScreen(username = username, password = password)
+    LoginScreen(
+        username = username,
+        setUsername = setUsername,
+        password = password,
+        setPassword = setPassword
+    )
 }
 
 @Composable
 fun LoginScreen(
-    username: MutableState<String>,
-    password: MutableState<String>
+    username: String,
+    setUsername: (String) -> Unit,
+    password: String,
+    setPassword: (String) -> Unit,
 ){
     Column {
-        TextWithInput(text = username)
+        LoginForm(username, setUsername, password, setPassword)
+
         Spacer(modifier = Modifier.size(15.dp))
-        TextWithInput(text = password)
+
+        Button(onClick = {
+            //save data
+        }) {
+
+            Text(text= stringResource(id = R.string.acceptar))
+        }
     }
 }
 
 @Composable
-fun TextWithInput(text: MutableState<String>) {
+private fun LoginForm(
+    username: String,
+    setUsername: (String) -> Unit,
+    password: String,
+    setPassword: (String) -> Unit
+) {
+    TextWithInput(
+        text = username,
+        setText = setUsername
+    )
+
+    Spacer(modifier = Modifier.size(15.dp))
+
+    TextWithInput(
+        text = password,
+        setText = setPassword
+    )
+}
+
+@Composable
+fun TextWithInput(text: String, setText: (String) -> Unit) {
     Row {
         Text(
             text = "username: ",
             modifier = Modifier
         )
         TextField(
-            value = text.value,
-            onValueChange = {
-                text.value = it
-            },
+            value = text,
+            onValueChange = { newText->  setText (newText) },
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Search,
             textStyle = TextStyle(color = Color.DarkGray),
