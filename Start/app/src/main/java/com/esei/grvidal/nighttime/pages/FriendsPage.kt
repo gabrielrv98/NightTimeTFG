7454eacc3969162e02804caa61884482594ba9f6
Gabriel Rguez Vidal <gabrielrv98@gmail.com>
2021-04-14 21:51:45 +0200

    Fixes search users

---
+++ Start/app/src/main/java/com/esei/grvidal/nighttime/pages/FriendsPage.kt
@@ -0,0 +1,222 @@
+package com.esei.grvidal.nighttime.pages
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PersonAdd
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.onCommit
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageAsset
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.esei.grvidal.nighttime.scaffold.NavigationScreens
+import androidx.navigation.NavHostController
+import androidx.ui.tooling.preview.Preview
+import com.esei.grvidal.nighttime.CustomDialog
+import com.esei.grvidal.nighttime.R
+import com.esei.grvidal.nighttime.data.*
+import com.esei.grvidal.nighttime.navigateWithId
+
+
+private const val TAG = "FriendsPage"
+
+/**
+ * Composable that manages the View of the list of chats
+ *
+ * @param navController  controller of hte navigation, its used to go back or navigate to other views
+ */
+@Composable
+fun FriendsPage(navController: NavHostController, chatVM: ChatViewModel) {
+
+    onCommit(chatVM.getId()) {
+
+        chatVM.getChats()
+
+    }
+
+    if (chatVM.showDialog) {
+        CustomDialog(
+            onClose = { chatVM.setDialog(false) }
+        ) {
+            FriendsSearch(
+                addUser = chatVM::addUser,
+                onSearch = chatVM::searchUsers,
+                users = chatVM.userList
+            )
+        }
+    }
+
+    FriendsScreen(
+        chatList = chatVM.chatList
+    ) { chatId ->
+        navController.navigateWithId(
+            NavigationScreens.ChatConversation.route,
+            chatId
+        )
+    }
+
+}
+
+@Composable
+fun FriendsScreen(
+    chatList: List<ChatFullView>,
+    onChatClick: (Long) -> Unit
+) {
+    Log.d(TAG, "FriendsScreen: photoList $chatList")
+    Column {
+        Header(
+            text = "Chat",
+            modifier = Modifier.padding(top = 24.dp)
+        )
+
+        LazyColumnFor(items = chatList) { chatData ->
+            ChatEntry(
+                userName = chatData.userNickname,
+                lastMessage = chatData.messages[0].text,
+                img = chatData.img,
+                onEntryClick = { onChatClick(chatData.friendshipId) }
+            )
+        }
+
+    }
+}
+
+
+/**
+ * Composable that shows the information about the chat
+ */
+@Composable
+fun ChatEntry(
+    userName: String,
+    lastMessage: String,
+    img: ImageAsset? = null,
+    onEntryClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 12.dp)
+            .padding(bottom = 8.dp)
+            .clickable(onClick = onEntryClick)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 6.dp)
+        ) {
+            Surface(
+                shape = RoundedCornerShape(50),
+                modifier = Modifier.preferredSize(45.dp).align(Alignment.CenterVertically),
+                color = Color.Gray
+            ) {
+                Log.d(
+                    TAG,
+                    "ChatEntry, timeing: lasRecomposition user $userName, img ${img != null} "
+                )
+
+
+                img?.let {
+                    Image(
+                        asset = it,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(start = 6.dp)
+            ) {
+                Text(
+                    text = userName,
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = lastMessage, style = MaterialTheme.typography.body2, maxLines = 1)
+            }
+        }
+        Divider(startIndent = 25.dp)
+    }
+
+}
+
+
+@Composable
+fun FriendsSearch(
+    addUser: (Long) -> Unit,
+    onSearch: (String) -> Unit,
+    users: List<UserSnapImage>
+) {
+
+    val (nicknameSearch, setNicknameSearch) = remember { mutableStateOf(TextFieldValue()) }
+    val state = rememberLazyListState()
+
+    Log.d(TAG, "FriendsSearch: userList = $users")
+
+    Column {
+        TextChanger(
+            modifier = Modifier.wrapContentSize(),
+            title = stringResource(R.string.search_friend),
+            canContainSpace = true,
+            value = nicknameSearch,
+            setValue = { textFV ->
+                setNicknameSearch(textFV)
+                onSearch(textFV.text)
+            },
+            canBeEmpty = true
+        )
+        Divider()
+
+        FriendlyUsersDialog(
+            itemsUser = users,
+            state = state
+        )
+        /*{ userId ->
+            Icon(
+                asset = Icons.Rounded.PersonAdd,
+                modifier = Modifier
+                    .clickable(onClick = {
+                        addUser(userId)
+                    })
+            )
+        }
+         //todo Composable doesn't show on screen it might be down
+         */
+    }
+}
+
+@Preview
+@Composable
+fun previewSearch() {
+    MaterialTheme {
+        FriendsSearch(addUser = {}, onSearch = {}, users = listOf(
+            UserSnapImage(1, "Nuria", "pinknut", null),
+            UserSnapImage(1, "Miguel", "emikepick", null),
+            UserSnapImage(1, "Maria", "dulceFlor", null),
+            UserSnapImage(1, "Marcos", "Tigre", null),
+            UserSnapImage(1, "Laura", "laux21", null),
+            UserSnapImage(1, "Sara", "saraaldo", null),
+            UserSnapImage(1, "Julio", "itsme", null),
+            UserSnapImage(1, "Juan", "john32", null),
+            UserSnapImage(1, "Pedro", "pcsantiago", null),
+            UserSnapImage(1, "Salva", "Salvador", null)
+        )
+        )
+    }
+}
